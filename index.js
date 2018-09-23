82ca8c538751a7164f7473fe1ab5316a01c9da2b
Felipe Wills <fwmartirani@gmail.com>
2018-09-23 13:32:54 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+﻿const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+
+res.send({ hi: 'there'});
+
+});
+const PORT = process.enve.PORT || 5000;
+
+
+app.listen(PORT);
